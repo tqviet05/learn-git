e8c2f7c83d61dd8b18743e650d634af8262c41e5
quocviet <tangquocviet@gmail.com>
2022-05-03 13:46:44 +0700

    add function export csv xlsx

--- crawler/app.rb
+++ crawler/app.rb
@@ -10,7 +10,9 @@ require './config/capybara'
 require './operation/crawl/helper_crawl'
 require './operation/crawl/site_generatedata_crawl'
 require './operation/generate/table_html'
-
+require 'sinatra'
+require 'csv'
+require 'axlsx'
 
 # Crawl data
 crawl = Operation::Crawl::SiteGeneratedataCrawl.new
@@ -19,5 +21,19 @@ crawl.start do
   html_saved_at = './publics/user_datatable.html'
   generate = Operation::Generate::TableHtml.new(crawl.file_downloaded, html_saved_at)
   generate.generate
+  generate.convert_json_csv
+  generate.convert_json_xls
   p "Generate HTML: #{html_saved_at}"
+end
+
+get '/' do
+  j_file = File.open('./publics/user_datatable.html')
+end
+
+get '/csv' do
+  send_file "./publics/data.csv" , :filename => 'data.csv'
+end
+
+get '/xls' do
+  send_file './publics/data.xlsx' , :filename => 'data.xls' 
 end
--- crawler/operation/generate/table_html.rb
+++ crawler/operation/generate/table_html.rb
@@ -4,10 +4,8 @@ module Operation
       def initialize (file_input , saved_at= "./publics/user_datatable.html")
         @file_input = file_input
         @saved_at = saved_at
-        # binding.pry
       end
     
-
       def generate
         erb_file = './templates/table.html.erb'
         erb_str = File.read(erb_file)
@@ -15,6 +13,54 @@ module Operation
         result = renderer.result(binding)
         # create file html 
         File.open(@saved_at, 'w') {|f| f.write(result)}
+      end
+
+      def convert_json_csv
+        # Pass 1: Collect headings
+        headings = Set.new
+        @file_input.each do |hash|
+          headings.merge(hash.keys)
+        end
+        # Pass 2: Fill data into the headings
+        csv_string = CSV.open('./publics/data.csv', "wb") do |csv|
+          csv << headings # append headings
+          @file_input.each do |hash|
+            row = {}
+            headings.each do |heading|
+              row[heading] = nil
+            end
+            hash.each do |k,v|
+              row[k] = v.to_s.gsub(/\r\n?/, "").delete("\n").delete("\r")
+            end
+            csv << row.values
+          end
+        end
+      end
+
+      def convert_json_xls
+        Axlsx::Package.new do |p|
+          p.workbook.add_worksheet(:name => "data") do |sheet| 
+            # Pass 1: Collect headings
+            headings = Set.new
+            @file_input.each do |hash|
+              headings.merge(hash.keys)
+            end
+            sheet.add_row headings.to_a
+            # Pass 2: Fill data into the headings
+            @file_input.each do |hash|
+              row = {}
+              headings.each do |heading|
+                row[heading] = nil
+              end
+              hash.each do |k,v|
+                row[k] = v.to_s.gsub(/\r\n?/, "").delete("\n").delete("\r")
+              end
+            sheet.add_row row.values
+            end
+            
+          end
+          p.serialize('./publics/data.xlsx', 'w+')
+        end
       end
     end
   end
